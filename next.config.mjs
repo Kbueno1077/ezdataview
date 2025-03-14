475cb6f04d9ba521acb767216f188fae72e2e6cc
kbueno1077 <kbueno1077@gmail.com>
2025-03-13 23:23:25 -0400

    Enhance Next.js configuration by adding file URL handling for improved module path resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  webpack: (config, {}) => {
+  webpack: (config) => {
     config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
